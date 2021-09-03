5d01daf6752927e1d9a8ebbbf1fe5a298f0dafb0
Leon Schreiber <leonschreiber96@gmail.com>
2021-09-03 16:26:07 +0200

    Initial commit

---
+++ GroceryTracker.Frontend/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+   <div class="home">
+      <img alt="Vue logo" src="../assets/logo.png" />
+      <hello-world />
+   </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue";
+
+@Options({
+   components: {
+      HelloWorld,
+   },
+})
+export default class Home extends Vue {}
+</script>
